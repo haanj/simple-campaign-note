f58a3b28482a490a60d62d427b9614eafe2bcdc9
Joshua Haan <joshua.haan@ymail.com>
2018-10-16 19:59:17 -0700

    Updates

---
+++ precache-manifest.efab3aabf4391ad5c7686102fded4660.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd0e4f0f0a5d6c12d97b",
+    "url": "/simple-campaign-note/static/js/runtime~main.fd0e4f0f.js"
+  },
+  {
+    "revision": "85090fc7054fc20f7e34",
+    "url": "/simple-campaign-note/static/js/main.85090fc7.chunk.js"
+  },
+  {
+    "revision": "5e502be75909774fb73d",
+    "url": "/simple-campaign-note/static/js/1.5e502be7.chunk.js"
+  },
+  {
+    "revision": "85090fc7054fc20f7e34",
+    "url": "/simple-campaign-note/static/css/main.7c685a93.chunk.css"
+  },
+  {
+    "revision": "af930c6503fa5d00e9477024b78fe94e",
+    "url": "/simple-campaign-note/index.html"
+  }
+];
